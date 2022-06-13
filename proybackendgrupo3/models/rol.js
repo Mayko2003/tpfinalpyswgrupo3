823607d068ea5636064aeebb8091cee6bc67e16b
luisrodri45 <luisedurodriguez2014@gmail.com>
2022-06-13 20:07:54 -0300

    modelo de area y rol agregados

---
+++ proybackendgrupo3/models/rol.js
@@ -0,0 +1,13 @@
+// import modulos
+const mongoose = require('mongoose');
+const Persona = require('./persona');
+const { Schema } = mongoose; // para poder usar el schema de mongoose
+
+// definir el esquema de la persona
+const rolSchema = new Schema({
+    nombre: { type: String, required: true },
+    personas: [{type:Schema.Types.ObjectId,ref:Persona}]
+})
+
+// exportar el esquema
+module.exports = mongoose.models.Rol || mongoose.model('Rol', rolSchema);
