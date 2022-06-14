8a5bee6666eb68bbf3aa1af0469d852f17a4eb74
santyXD905 <santiago24080218@gmail.com>
2022-06-14 00:09:13 -0300

    1er commit: añadi el modelo, el servicio, la componente de persona y puse algunos servicios

--- proyfrontendgrupo3/src/app/app.module.ts
+++ proyfrontendgrupo3/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonaComponent } from './components/persona/persona.component';
+import { HeaderComponent } from './components/layouds/header/header.component';
+import { FooterComponent } from './components/layouds/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonaComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ proyfrontendgrupo3/src/app/services/persona.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Persona } from '../models/persona';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class PersonaService {
+  
+  urlBase: string = 'http://localhost:3000/api'
+  constructor(private _http: HttpClient) { }
+
+  //servicio para agregar una persona
+  public addPersona(persona: Persona): Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json"
+      })
+    }
+    let body = JSON.stringify(persona)
+    return this._http.post(this.urlBase, body, httpOptions)
+  }
+
+  //servicio para eliminar una persona
+  public deletePasaje(persona: Persona): Observable<any>{
+    const httpOptions = {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json"
+      })
+    }
+    return this._http.delete(this.urlBase+"/"+persona._id, httpOptions)
+  }
+
+  //servicio para actualizar una persona
+  public updatePasaje(persona: Persona): Observable<any>{
+    const httpOptions = {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json"
+      })
+    }
+    let body = JSON.stringify(persona)
+
+    return this._http.put(this.urlBase+"/"+persona._id, body, httpOptions)
+  }
+
+  //servicio para enviar todas las personas
+  public getPersonas(): Observable<any>{
+    const httpOptions = {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json"
+      })
+    }
+    return this._http.get(this.urlBase, httpOptions)
+  }
+
+  //servicio enviar una persona segun su id
+  public getPersonabyID(_id:string): Observable<any>{
+    const httpOptions = {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json"
+      })
+    }
+    return this._http.get(this.urlBase+"/"+_id, httpOptions)
+  }
+}
