f5a5c81165bf345060eb4dc13a69427d7fb4586b
luisrodri45 <luisedurodriguez2014@gmail.com>
2022-06-22 00:18:35 -0300

    modelo de recurso y medioTransmision agregados

---
+++ proybackendgrupo3/models/recurso.js
@@ -0,0 +1,13 @@
+// import modulos
+const mongoose = require('mongoose');
+const Anuncio = require('./anuncio');
+const { Schema } = mongoose; // para poder usar el schema de mongoose
+
+const recursoSchema = new Schema({
+    tipo: { type:String, required: true},
+    recurso : { type: String, required: true},
+    anuncio : { type : Schema.Types.ObjectId, ref: Anuncio, required: true}
+})
+
+//exportar el esquema
+module.exports = mongoose.models.Recurso || mongoose.model('Recurso', recursoSchema);
