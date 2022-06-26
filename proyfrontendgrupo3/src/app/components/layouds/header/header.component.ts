e0e4bff2a401d3bfd4e7e5afc60ae27cd3b6014c
Samas1503 <samueleliasparedes.10@gmail.com>
2022-06-26 15:41:02 -0300

    Samuel Paredes -> Se modifico el header haciendo que sea diferente para el administrador, aun falta mejorarlo

--- proyfrontendgrupo3/src/app/components/layouds/header/header.component.ts
+++ proyfrontendgrupo3/src/app/components/layouds/header/header.component.ts
@@ -4,21 +4,23 @@ import { LoginService } from 'src/app/services/login.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-
-  roles!: any
+  roles!: any;
+  userImg!: string;
 
   constructor(public loginService: LoginService) {
     this.roles = loginService.rolLogged();
+    if (this.roles)
+      if (this.roles[0] == 'administrador')
+        this.userImg = 'assets/images/admin.png';
+      else this.userImg = 'assets/images/default-user.jpg';
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  logout(){
+  logout() {
     this.loginService.logout();
   }
-
 }
