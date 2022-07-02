29761403ee121fac9b56543c8fa4fda1b3d01b4d
Santiago Tintilay <santiago24080218@gmail.com>
2022-07-02 12:35:00 -0300

    realizado guardar anuncio, falta corregir estados al guardar e editar

--- proyfrontendgrupo3/src/app/components/recursos-anuncio/recursos-anuncio.component.ts
+++ proyfrontendgrupo3/src/app/components/recursos-anuncio/recursos-anuncio.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Anuncio } from 'src/app/models/anuncio';
+import { AnuncioService } from 'src/app/services/anuncio.service';
 
 @Component({
   selector: 'app-recursos-anuncio',
@@ -8,15 +10,22 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class RecursosAnuncioComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) { }
+  anuncio!: Anuncio
+  constructor(private route: ActivatedRoute,private anuncioService: AnuncioService) { 
+    this.anuncio = new Anuncio()
+  }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       if (params['id']) {
-        var a = params['id'];
-        console.log(a);
+        var id = params['id'];
+        this.anuncioService.getAnuncio(id).subscribe(res => {
+          this.anuncio = res
+        })
       }
     })
   }
+
+
 
 }
