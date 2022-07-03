f9d3c25fd8533900e64aa61e8e1980db00e5e5b2
= <=>
2022-07-03 17:45:54 -0300

    agregando nodemailer

---
+++ proybackendgrupo3/controllers/email.controller.js
@@ -0,0 +1,32 @@
+const nodemailer = require('nodemailer');
+
+const emailController = {}
+
+emailController.sendEmail = async(req, res) => {
+    const email = req.body.email;
+    const text = req.body.text;
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'quequeres12@gmail.com',
+            pass: 'QWERTqwert12345'
+        }
+    });
+    
+    const mailOptions = {
+        from: 'quequeres12@gmail.com',
+        to: email,
+        subject: 'Nuevo Anuncio',
+        text: text
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    }
+    );
+}
